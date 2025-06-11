54c4bc87f248fca37f32b1ba2c9fef0b040f2dcb
Danila <kallaurdanila13@gmail.com>
2025-06-11 18:51:42 +0300

    latext working version on 18:51 11.06

---
+++ src/model.js
@@ -0,0 +1,15 @@
+
+import image from './assets/image.png'
+
+ export const model = [
+    {type: 'title' , value: 'Hello world from JS', options:{
+        tag:'h2'
+    }},
+    {type: 'text' , value: 'here text'},
+    {type: 'columns' , value: [
+        '11111111111',
+        '22222222222',
+        '33333333333'
+    ]},
+    {type: 'image', value: image}
+]

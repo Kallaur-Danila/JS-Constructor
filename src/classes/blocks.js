5a24abca48960d26e3c8d8b3fbafaeade75b9e45
Danila <kallaurdanila13@gmail.com>
2025-06-13 13:51:30 +0300

    last vorking version 13:51 13.06

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -1,8 +1,12 @@
+import {row, col, css} from '../utils'
 class Block {
     constructor(type,value,options){
         this.type = type
         this.value = value
         this.options = options
+    }
+    toHTML(){
+        throw new Error('метод toHTML должен быть реализован')
     }
 }
 
@@ -10,19 +14,37 @@ export class TitleBlock extends Block {
     constructor(value, options){
         super('title', value, options)
     }
+
+    toHTML(){
+        const {tag = 'h1', styles} = this.options
+        return row(col(`<${tag}>${this.value}</${tag}>`), css(styles))
+    }
 }
 export class ImageBlock extends Block {
     constructor(value, options){
         super('image', value, options)
     }
+    toHTML(){
+        return row(`<img src = "${this.value}" />`,  css(this.options.styles))
+    }
 }
+
 export class ColumnsBlock extends Block {
     constructor(value, options){
         super('columns', value, options)
     }
+
+    toHTML(){
+        const html = this.value.map(col).join('')
+        return row(html,  css(this.options.styles))
+    }
 }
+
 export class TextBlock extends Block {
     constructor(value, options){
         super('text', value, options)
+    }
+    toHTML(){
+        return row(col(`<p>${this.value}</p>`), css(this.options.styles))
     }
 }
--- src/templates.js
+++ src/templates.js
@@ -1,27 +1,30 @@
+import { row, col, css } from './utils'
 
-import {row, col, css} from './utils'
-
- function title(block){
-    const {tag = 'h1', styles} = block.options
-    return row(col(`<${tag}>${block.value}</${tag}>`), css(styles))
+export function title(block) {
+  const tag = block.options.tag || 'h1'
+  return row(col(`<${tag}>${block.value}</${tag}>`), css(block.options.styles))
 }
 
- function text(block){
-    return row(col(`<p>${block.value}</p>`), css(block.options.styles))
+export function text(block) {
+  return row(col(`<p>${block.value}</p>`), css(block.options.styles))
 }
 
- function columns(block){
-    const html = block.value.map(col).join('')
-    return row(html,  css(block.options.styles))
+export function columns(block) {
+  const html = block.value.map(col).join('')
+  return row(html, css(block.options.styles))
 }
 
-export function image(block){
-    return row(`<img src = "${block.value}" />`,  css(block.options.styles))
+export function image(block) {
+  const { imageStyles, alt, styles } = block.options
+  return row(
+    `<img src="${block.value}" alt="${alt}" style="${css(imageStyles)}" />`,
+    css(styles)
+  )
 }
 
-export const templates =  {
-    title,
-    text,
-    image,
-    columns
+export const templates = {
+  title,
+  text,
+  image,
+  columns
 }
